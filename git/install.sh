e681b146a7f6d89f470acd70486ac4c87d4d1376
Chris Lee <chrismlee42@gmail.com>
2023-07-19 04:15:50 -0500

    Actually link the git config files instead of echoing the commands

--- git/install.sh
+++ git/install.sh
@@ -4,7 +4,7 @@ set -e
 
 for configPath in ${DOTFILES_LOCATION}/git/.gitconfig.*; do
     config=$(echo $configPath | sed "s|$DOTFILES_LOCATION/git/||")
-    echo ln -sf "${DOTFILES_LOCATION}/git/${config}" "${HOME}/${config}"
+    ln -sf "${DOTFILES_LOCATION}/git/${config}" "${HOME}/${config}"
 done
 
-echo ln -sf "${DOTFILES_LOCATION}/git/.gitconfig" "${HOME}/.gitconfig"
+ln -sf "${DOTFILES_LOCATION}/git/.gitconfig" "${HOME}/.gitconfig"
