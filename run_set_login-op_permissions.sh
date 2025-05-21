63fa68dc3bb5fbe59fb0ac400018f28e62c4b54f
Chris Lee <chrismlee42@gmail.com>
2025-05-21 07:28:08 +0000

    Go back to having script fix login-op permissions and add ssh identities to login-op script

--- run_set_login-op_permissions.sh
+++ run_set_login-op_permissions.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-FILE="$HOME/.local/bin/login-op"
-if [ -f "$FILE" ]
-then
-    if [ "$(stat -c %a "$FILE")" != "750" ]
+declare -A FILE_MODES=(
+    ["$HOME/.local/bin/login-op"]="700"
+)
+
+for FILE in "${!FILE_MODES[@]}"
+do
+    # echo "FILE is \"$FILE\""
+    MODE=${FILE_MODES[$FILE]}
+    # echo "MODE is is \"$MODE\""
+    if [ -f "$FILE" ]
     then
-        chmod 750 "$FILE"
+        if [ "$(stat -c %a "$FILE")" != $MODE ]
+        then
+            # echo "changing file mode for $FILE"
+            chmod $MODE "$FILE"
+        fi
     fi
-fi
+done
