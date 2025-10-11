cba9e918249ad914063586b07bb9e9c78db32419
Chris Lee <labmonkey42@gmail.com>
2025-10-11 21:40:31 +0000

    Move oh-my-posh installation on Windows to run_once script

--- install.ps1
+++ install.ps1
@@ -6,11 +6,6 @@ if (!(Get-Command chezmoi -errorAction SilentlyContinue))
     Write-Output "Installing chezmoi"
     winget install twpayne.chezmoi
 }
-if (!(Get-Command oh-my-posh -errorAction SilentlyContinue))
-{
-    Write-Output "Installing oh-my-posh"
-    winget install JanDeDobbeleer.OhMyPosh
-}
 
 Write-Output "Running chezmoi"
 ~/bin/chezmoi init --apply "$PSScriptRoot"
